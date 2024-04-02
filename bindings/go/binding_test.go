6b9f0f4bcc4360ea11c9c51320d8be1132509c77
Stephan Seitz <sseitz@nvidia.com>
2024-04-02 23:47:06 +0200

    build: bump tree-sitter-cli to v0.22.2, add more bindings

---
+++ bindings/go/binding_test.go
@@ -0,0 +1,15 @@
+package tree_sitter_hlsl_test
+
+import (
+	"testing"
+
+	tree_sitter "github.com/smacker/go-tree-sitter"
+	"github.com/tree-sitter/tree-sitter-hlsl"
+)
+
+func TestCanLoadGrammar(t *testing.T) {
+	language := tree_sitter.NewLanguage(tree_sitter_hlsl.Language())
+	if language == nil {
+		t.Errorf("Error loading Hlsl grammar")
+	}
+}
---
+++ bindings/python/tree_sitter_hlsl/__init__.py
@@ -0,0 +1,5 @@
+"Hlsl grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
